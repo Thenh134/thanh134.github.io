f614392f280dbcc6927e4fc76f94a079388685a5
Thenh134 <thanhthpt2021@gmail.com>
2023-10-02 22:08:34 +0700

    first commit

---
+++ agent/Agent.java
@@ -0,0 +1,21 @@
+package agent;
+
+import agent.Action;
+
+public class Agent {
+	private AgentProgram program;
+
+	public Agent() {
+	}
+
+	public Agent(AgentProgram aProgram) {
+		program = aProgram;
+	}
+
+	public Action execute(Percept p) {
+		if (program != null) {
+			return program.execute(p);
+		}
+		return NoOpAction.NO_OP;
+	}
+}
